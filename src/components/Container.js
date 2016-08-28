efeb83290aad33fbe4e55a42cdd4945ab1406039
Tom <humantom88@yandex.ru>
2016-08-28 08:33:22 +0300

    0011# Lesson 5  homework (merging lesson changes)

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,20 +1,21 @@
 import React, { Component, PropTypes } from 'react'
 import ArticleList from './ArticleList'
-/*import Select from 'react-select'
+import Select from 'react-select'
 import 'react-select/dist/react-select.css'
-import JqueryComponent from './JqueryComponent'*/
+import JqueryComponent from './JqueryComponent'
+import DaypickerContainer from './DaypickerContainer'
+import Counter from './Counter'
 import { findDOMNode } from 'react-dom'
-import DateRangePicker from './DateRangePicker'
+import { connect } from 'react-redux'
 
 class Container extends Component {
     static propTypes = {
 
-    }
+    };
 
-    /*state = {
-        selected: null,
-        selectedDateRange: null
-    }*/
+    state = {
+        selected: null
+    }
 
     render() {
         const options = this.props.articles.map(article => ({
@@ -23,24 +24,29 @@ class Container extends Component {
         }))
         return (
             <div>
-                {/*<Select options = {options} value={this.state.selected} onChange = {this.handleChange} multi={true}/>*/}
-                <DateRangePicker />
+                <Counter />
                 <ArticleList articles = {this.props.articles} />
-                {/*<JqueryComponent items = {this.props.articles} ref={this.getJQ}/>*/}
+                <Select options = {options} value={this.state.selected} onChange = {this.handleChange} multi={true}/>
+                <DaypickerContainer />
+                <JqueryComponent items = {this.props.articles} ref={this.getJQ}/>
             </div>
         )
     }
-    
-    /*getJQ = (ref) => {
+
+    getJQ = (ref) => {
         this.jqRef = ref
         console.log('---', findDOMNode(ref))
-    }*/
+    }
 
-    /*handleChange = (selected) => {
+    handleChange = (selected) => {
         this.setState({
             selected
         })
-    }*/
+    }
 }
 
-export default Container
+export default connect((state) => {
+        const { articles } = state
+        return { articles }
+    }
+)(Container)
